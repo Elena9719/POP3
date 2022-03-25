2406c5b51b06a236b4d23bd782c0299dc45762a4
Elena9719 <elen.9719@gmail.com>
2022-03-25 17:10:07 +0200

    Pop3

---
+++ pop3/pop3lab1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Limilabs.Mail;
+using Limilabs.Client.POP3;
+
+namespace pop3 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Pop3 pop3 = new Pop3())
+            {
+                pop3.Connect("elen.9719@gmail.com");  // or ConnectSSL for SSL
+                pop3.UseBestLogin("elen.9719@gmail.com", "yscjedicjgkchnjb");
+
+                // Receive all messages and display the subject
+                MailBuilder builder = new MailBuilder();
+                foreach (string uid in pop3.GetAll())
+                {
+                    IMail email = builder.CreateFromEml(
+                      pop3.GetMessageByUID(uid));
+
+                    Console.WriteLine(email.Subject);
+                    Console.WriteLine(email.Text);
+                }
+                pop3.Close();
+            }
+        }
+    }
+}
+
+
---
+++ pop3/pop3lab1/pop3.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Limilabs.Mail;
+using Limilabs.Client.POP3;
+using System.Windows;
+
+namespace pop3 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Pop3 pop3 = new Pop3())
+            {
+                pop3.ConnectSSL("pop.gmail.com", 995);  // or ConnectSSL for SSL
+                pop3.UseBestLogin("sdgjluit9@gmail.com", "password");
+
+                // Receive all messages and display the subject
+                MailBuilder builder = new MailBuilder();
+                foreach (string uid in pop3.GetAll())
+                {
+                    IMail email = builder.CreateFromEml(
+                      pop3.GetMessageByUID(uid));
+
+                    Console.WriteLine(email.Subject);
+                    Console.WriteLine(email.Text);
+                }
+                pop3.Close();
+            }
+          
+        }
+    }
+}
+
+
